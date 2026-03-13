8819a2d787501cd3fec343236bd66215c2d96b66
DenisLeshukov <leshukov80@list.ru>
2026-03-13 14:30:40 +0500

    first

---
+++ GeometryShape/Triangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryShape
+{
+    public class Triangle :Shape
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+        public override double Area ()
+        {
+            throw new NotImplementedException( );
+        }
+
+        public override double Perimeter ()
+        {
+            throw new NotImplementedException( );
+        }
+
+        public override string ToString ()
+        {
+            throw new NotImplementedException( );
+        }
+    }
+}
