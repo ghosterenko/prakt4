abe4df781065e1a43bafe974e31e778d5297aaa0
ghosterenko <arsenijkonkov243@gmail.com>
2026-03-13 15:43:00 +0500

    triangleTest

---
+++ triangle/TriangleTest.cs
@@ -0,0 +1,27 @@
+using GeometryShape;
+namespace triangle
+{
+    public class triangleTest
+    {
+        [Fact]
+        public void CheckMethodAreaForTriangle()
+        {
+            Triangle triangle = new Triangle( );
+            triangle.SideA = 13;
+            triangle.SideB = 14;
+            triangle.SideC = 15;
+            var result = 84;
+            Assert.Equal(result, triangle.Area());
+        }
+        [Fact]
+        public void CheckMethodPerimeterForTriangle()
+        {
+            Triangle triangle = new Triangle( );
+            triangle.SideA = 11;
+            triangle.SideB = 10;
+            triangle.SideC = 10;
+            var result = 30;
+            Assert.Equal(result, triangle.Perimeter());
+        }
+    }
+}
